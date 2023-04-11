cc8a8c685ec035c035f3feb54b023bb8fcf993fb
Fernando Parra <thefer767@gmail.com>
2023-04-11 17:39:18 -0400

    Analizador Sintactico

--- analizador.cpp
+++ analizador.cpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include <cstdlib>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 //Variables
@@ -12,7 +14,19 @@ string output = "TOKEN\tLEXENA\n";
 string checkspace = "";
 string palabra = "";
 string caracter = "";
+std::vector< char > Palabras;
+std::vector< int > arr;
+std::string words[500];
+int wordsi = 0;
+int wtf = 0;
 int saltar = 1;
+bool error = false;
+int parentopen = 0;
+int parentclose = 0;
+bool abrirexp = false;
+bool cerrarexp = false;
+bool igualdad = false;
+int prueba = 0;
 
 //funcion para comprobar numeros de mas de un digito
 bool isNumber(const string& s)
@@ -25,7 +39,20 @@ bool isNumber(const string& s)
 }
 
 //Codigo que se va a compilar
-string codigo = "VARIABLES variable1=25. variable2=30. variable3=84. BEGIN resultado=variable1/(variable2*variable3). END";
+string codigo = "VARIABLES x=5. y=10. w = 25. BEGIN op = x+(y*w). END";
+
+
+void red () {
+    printf("\033[1;31m");
+    }
+
+void reset () {
+  printf("\033[0m");
+}
+
+void green () {
+  printf("\033[0;32m");
+}
 
 //inicio
 int main(){
@@ -34,7 +61,9 @@ int main(){
     //cargar codigo
     std::string str = codigo;
 
+//ANALIZADOR LEXICO
     //LEE CADA DIGITO DEL CODIGO
+    
 for(std::string::size_type i = 0; i < str.size(); ++i) {
     //variable para revisar el siguiente caracter
     checkspace = str[i+1];
@@ -61,40 +90,58 @@ for(std::string::size_type i = 0; i < str.size(); ++i) {
         caracter= "";
 
  //verifica la palabra leida
+ wtf++;
         if (palabra == "BEGIN" || palabra == "END" || palabra == "VARIABLES")
         {
             output += '\t';
             output += "PR";
+            words[wordsi]=palabra;
+            wordsi++;
             palabra = "";
         }else{
             if (palabra == ".")
         {
             output += '\t';
             output += "Finalizador";
+            
+            words[wordsi]=palabra;
+            wordsi++;
             palabra = "";
         }else{
             if (palabra == "=")
         {
             output += '\t';
             output += "Asignacion";
+            
+            words[wordsi]=palabra;
+            wordsi++;
             palabra = "";
         }else{
             if (palabra == "+" || palabra == "-" || palabra == "/" || palabra == "*" || palabra == "^")
         {
             output += '\t';
             output += "OP_Airmetico";
+            
+            words[wordsi]=palabra;
+            wordsi++;
             palabra = "";
         }else{
             if (palabra == "(" || palabra == ")")
         {
             output += '\t';
             output += "Agrupador";
+            
+            words[wordsi]=palabra;
+            wordsi++;
             palabra = "";
         }else{
             if (palabra == "0" || palabra == "1" || palabra == "2" || palabra == "3" || palabra == "4" || palabra == "5" || palabra == "6" || palabra == "7" || palabra == "8"|| palabra == "9")
         {
             output += '\t';
             output += "NUM";
+            
+            words[wordsi]="NUM";
+            wordsi++;
             palabra = "";
         }else{
             //comprueba si la palabra es un numero
@@ -102,6 +149,9 @@ for(std::string::size_type i = 0; i < str.size(); ++i) {
             {
             output += '\t';
             output += "NUM";
+            
+            words[wordsi]="NUM";
+            wordsi++;
             palabra = "";
 
             //si no es un numero es un IDEN
@@ -109,6 +159,9 @@ for(std::string::size_type i = 0; i < str.size(); ++i) {
             {
             output += '\t';
             output += "IDEN";
+            
+            words[wordsi]="IDEN";
+            wordsi++;
             palabra = "";
             }
             
@@ -118,8 +171,10 @@ for(std::string::size_type i = 0; i < str.size(); ++i) {
 
         //realiza el salto de linea
         output += "\n";
+        
         //se limpia el valor de la palabra
         palabra = "";
+        
         //verifica si hay que realizar un salto
         if (saltar == 1)
         {
@@ -137,10 +192,198 @@ for(std::string::size_type i = 0; i < str.size(); ++i) {
     
 }
 
+
+
 //MOSTRAR RESULTADO
-printf("%s", output);
- 
+printf("%s \n", output);
+
+
+//ANALIZADOR SINTACTICO
+
+for (size_t i = 0; i < wtf; i++)
+{if (words[i]=="(")
+    {parentopen+=1;}
+    if (words[i]==")")
+    {parentclose+=1;}}
+
+
+for (size_t i = 0; i < wtf; i++)
+{
+    std::cout << words[i] << "\n";
+
+
+    if (words[i]=="=")
+    {
+        if (words[i-1]!="IDEN")
+        {
+            error=true;
+                red();
+                printf("Error de Sintaxis en la linea %d En el lado izquierdo de una igualdad debe de haber un IDEN. \n", i);
+                reset();
+                igualdad=false;
+        }
+        
+        igualdad=true;
+        for (size_t y = i+1; y < wtf; y++)
+        {
+                if (words[y]=="."){
+               igualdad=false;
+               break;
+            }else{
+            if (words[y]=="="&&igualdad==true)
+            {
+                error=true;
+                red();
+                printf("Error de Sintaxis en la linea %d Se espera el uso del finalizador . \n", i);
+                reset();
+                igualdad=false;
+                break;
+            }
+            }
+        }
+        if (igualdad==true)
+        {
+            error=true;
+                red();
+                printf("Error de Sintaxis en la linea %d Se espera el uso del finalizador . \n", i);
+                reset();
+        }
+        
+        // if (words[i+1]=="IDEN"||words[i+1]=="NUM")
+        // {
+        //     if (words[i+2]==".")
+        //     {
+
+        //     }else{
+        //     error=true;
+        //     red();
+        //     printf("Error de Sintaxis en la linea %d Se espera el uso del finalizador . \n", i);
+        //     reset();
+        //     }
+            
+        // }
+        
+    }
+    
+
+
+
+    if (words[i]=="IDEN")
+    {
+        
+        
+        if (words[i+1]!="="&&words[i+1]!="+"&&words[i+1]!="-"&&words[i+1]!="*"&&words[i+1]!="/"&&words[i-1]!="="&&words[i-1]!="+"&&words[i-1]!="-"&&words[i-1]!="*"&&words[i-1]!="/")
+        {
+            error=true;
+            red();
+            printf("Error de Sintaxis en la linea %d \n", i);
+            reset();
+        }
+        if (words[i+1]=="IDEN")
+        {
+            error=true;
+            red();
+            printf("Error de Sintaxis en la linea %d Hay 2 Identificadores seguidos\n", i);
+            reset();
+        }
+        
+    }
+    
+
+    if (words[i]=="+"||words[i]=="-"||words[i]=="*"||words[i]=="/")
+    {
+        if (words[i+1]=="+"||words[i+1]=="-"||words[i+1]=="*"||words[i+1]=="/")
+        {
+            error=true;
+            red();
+            printf("Error de Sintaxis en la linea %d Hay 2 operadores seguidos\n", i);
+            reset();
+        }
+
+        if (words[i+1]!="IDEN" || words[i-1]!="IDEN")
+        {
+            if (words[i+1]!="IDEN" || words[i-1]!="NUM")
+            {
+                if (words[i+1]!="NUM" || words[i-1]!="IDEN")
+            {
+                if (words[i+1]!="NUM" || words[i-1]!="NUM")
+            {
+            if (words[i+1]!="(" || words[i-1]!="NUM")
+            {
+            if (words[i+1]!="NUM" || words[i-1]!=")")
+            {
+            if (words[i+1]!="(" || words[i-1]!="IDEN")
+            {
+           if (words[i+1]!="IDEN" || words[i-1]!=")")
+            {
+            error=true;
+            red();
+            printf("Error de Sintaxis en la linea %d Uso incorrecto del operador\n", i);
+            reset();
+            }
+            }
+            }
+            }
+            }
+            }
+            }
+            
+            
+        }
+
+
+        
+    }
+
+    if (words[i]=="("|| words[i]==")")
+    {
+        if (parentopen!=parentclose)
+        {
+            error=true;
+            red();
+            printf("Error de Sintaxis en la linea %d No hay la misma cantidad de parentesis abiertos que cerrados\n", i);
+            reset();
+        }
+        
+    }
+    
+
+    if (words[i]=="END")
+    {
+        if(words[i+1]==""){
+        
+        }else{
+        error=true;
+        red();
+        printf("Error en la linea %d No puede haber codigo despues de la palabra reservada END \n", i);
+        reset();
+        }
+        
+    }
+
+  
+    }
+    
+
+      
+        
+
+    if (error==false)
+    {
+        green();
+        printf("El Codigo es Valido, ahora sera compilado \n");
+        reset();
+    }
+
+
+    
 //SOBREESCRIBE EL ARCHIVO
 fwrite(output.data(), sizeof(char), output.size(), file);
 
+    
+    
+
+    
 }
+
+
